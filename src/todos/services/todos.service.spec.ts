e426514d6b1b6d7552159f546cca8bbe02915616
Chris Kilmer <christopherkilmer@gmail.com>
2018-10-19 10:58:47 -0600

    Add specs for Todo service create.

--- src/todos/services/todos.service.spec.ts
+++ src/todos/services/todos.service.spec.ts
@@ -1,8 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { TodosService } from '@todos/services/todos.service';
+import { cold } from 'jasmine-marbles';
+import { HttpClient } from '@angular/common/http';
 
 describe('TodosService', () => {
+  let service: TodosService;
+  let http: HttpClient;
+
   const todos = [
     { id: 1, description: 'Todo #1', completed: false },
     { id: 2, description: 'Todo #2', completed: true }
@@ -14,15 +19,30 @@ describe('TodosService', () => {
       providers: [TodosService]
     });
 
-    const service: TodosService = TestBed.get(TodosService);
-    spyOn(service, 'getTodos').and.returnValue(todos);
+    http = TestBed.get(HttpClient);
+    service = TestBed.get(TodosService);
   });
 
   describe('getTodos', () => {
-    it('should get all Todos', () => {
-      const service: TodosService = TestBed.get(TodosService);
-
+    test('should get all Todos', () => {
+      spyOn(service, 'getTodos').and.returnValue(todos);
       expect(service.getTodos()).toEqual(todos);
+    });
+  });
+
+  describe('createTodo', () => {
+    test('should create a new Todo', () => {
+      const newTodo = { description: 'New Todo #1', completed: false };
+      const createdTodo = {
+        id: 1,
+        description: 'New Todo #1',
+        completed: false
+      };
+      const expected = cold('-a|', { a: createdTodo });
+      http.post = jest.fn(() => expected);
+
+      expect(service.createTodo(newTodo)).toBeObservable(expected);
+      expect(http.post).toHaveBeenCalledWith(service.baseUrl, newTodo);
     });
   });
 });
