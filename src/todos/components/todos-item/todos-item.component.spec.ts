89b0ad37c58e5475ac521f63079c6e7b2ebb6e87
Chris Kilmer <christopherkilmer@gmail.com>
2018-10-19 13:47:03 -0600

    Add specs for TodoItemComponent  update.

--- src/todos/components/todos-item/todos-item.component.spec.ts
+++ src/todos/components/todos-item/todos-item.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
 import { MaterialModule } from '@material/material.module';
-
 import { TodosItemComponent } from './todos-item.component';
 import * as todosReducers from '@todos/store/reducers/todos.reducers';
+import { UpdateTodo } from '@todos/store/actions/todos.actions';
 
 describe('TodosItemComponent', () => {
   let component: TodosItemComponent;
   let fixture: ComponentFixture<TodosItemComponent>;
+  let store: Store<todosReducers.TodosState>;
 
   const todo = { id: 1, description: 'Todo #1', completed: false };
 
@@ -24,10 +26,23 @@ describe('TodosItemComponent', () => {
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+
     fixture = TestBed.createComponent(TodosItemComponent);
     component = fixture.componentInstance;
     component.todo = todo;
     fixture.detectChanges();
+  });
+
+  test('should dispatch UpdateTodo on checkbox click', () => {
+    const action = new UpdateTodo({ ...todo, completed: !todo.completed });
+    const checkboxDe = fixture.debugElement.query(By.css('.toggle-completed'));
+    const checkboxEl = checkboxDe.nativeElement as HTMLInputElement;
+
+    checkboxEl.click();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
   });
 
   describe('html template', () => {
